3409882a42be6670f68d72809643af60484cb83a
gyu85 <gyu.will85@gmail.com>
2024-11-14 01:02:44 +0900

    [form] 팝업 전송 누를 때 유효성 검사

--- src/js/modal/DailyModal.js
+++ src/js/modal/DailyModal.js
@@ -35,6 +35,7 @@ DailyModal.prototype.create = function () {
   this.hurdleElement = document.querySelector('#hurdle');
   this.retrospectElement = document.querySelector('#retrospect');
   this.wrapHurdlesElement = document.querySelector('.wrap-hurdles');
+  this.dailyRecordForm = document.querySelector('#dailyRecord');
 
   RadioBox.onChange('input[name="spendTime"]', this.radioHandler.bind(this));
 
@@ -42,9 +43,38 @@ DailyModal.prototype.create = function () {
     removeHtml(this.modalLayout.element),
   );
 
+  onClick(this.modalLayout.btnFooterSave, this.submitHandler.bind(this));
   onKeydown(this.hurdleElement, this.hurdleInputHandler.bind(this));
-
   onClick(this.wrapHurdlesElement, this.tagDeleteHandler.bind(this));
+};
+
+DailyModal.prototype.submitHandler = function () {
+  const trackerHourElement = this.dailyRecordForm.querySelector('#trackerHour');
+  const trackerMinuteElement =
+    this.dailyRecordForm.querySelector('#trackerMinute');
+  const trackerWorkElement = this.dailyRecordForm.querySelector('#trackerWork');
+  const spendTimeCheckedElement = this.dailyRecordForm.querySelector(
+    'input[name="spendTime"]:checked',
+  );
+  const scoreElement = this.dailyRecordForm.querySelector('#score');
+  const retrospectElement = this.dailyRecordForm.querySelector('#retrospect');
+
+  if (!trackerWorkElement.value) {
+    alert('하는일 입력해 주세요.');
+    trackerWorkElement.focus();
+  }
+
+  if (!spendTimeCheckedElement) {
+    alert('소요 시간을 선택해주세요.');
+  }
+
+  this.dailyData.foncusedTime = `${trackerHourElement.value}시 ${trackerMinuteElement.value}분`;
+  this.dailyData.doing = trackerWorkElement.value;
+  this.dailyData.spendTime = spendTimeCheckedElement.value;
+  this.dailyData.focusScore = scoreElement.value;
+  this.dailyData.retrospect = retrospectElement.value;
+
+  // Data를 추가 하는 부분은 다음 PR에서 진행
 };
 
 DailyModal.prototype.tagDeleteHandler = function (event) {
@@ -93,97 +123,97 @@ DailyModal.prototype.radioHandler = function (event) {
 
 DailyModal.drawHtml = function () {
   return `
-      <form action="#none">
-        <div class="daily-record">
-          <div class="wrap-daily">
-            <strong class="daily-title">
+    <form name="dailyRecord" id="dailyRecord">
+      <div class="daily-record">
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <span class="txt-essential"
+              >*
+              <span class="blind">필수</span>
+            </span>
+            집중한 시간
+          </strong>
+          <div class="daily-detail">
+            ${SelectBox.drawHtml(hourData)}
+            <span class="txt-time">시</span>
+            ${SelectBox.drawHtml(minuteData)}
+            <span class="txt-time">분</span>
+          </div>
+        </div>
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <label for="trackerWork">
               <span class="txt-essential"
                 >*
                 <span class="blind">필수</span>
               </span>
-              집중한 시간
-            </strong>
-            <div class="daily-detail">
-              ${SelectBox.drawHtml(hourData)}
-              <span class="txt-time">시</span>
-              ${SelectBox.drawHtml(minuteData)}
-              <span class="txt-time">분</span>
-            </div>
+              하는 일</label
+            >
+          </strong>
+          <div class="daily-detail">
+            <input
+              type="text"
+              id="trackerWork"
+              class="tf-daily"
+              placeholder="지금 하는 일을 입력해 주세요." />
           </div>
-          <div class="wrap-daily">
-            <strong class="daily-title">
-              <label for="trackerWork">
-                <span class="txt-essential"
-                  >*
-                  <span class="blind">필수</span>
-                </span>
-                하는 일</label
-              >
-            </strong>
-            <div class="daily-detail">
-              <input
-                type="text"
-                id="trackerWork"
-                class="tf-daily"
-                placeholder="지금 하는 일을 입력해 주세요." />
-            </div>
-          </div>
-          <div class="wrap-daily">
-            <strong class="daily-title">
-              <span class="txt-essential"
-                >*
-                <span class="blind">필수</span>
-              </span>
-              소요시간
-            </strong>
-            <div class="daily-detail">
-              <div class="wrap-spend">
-                ${RadioBox.drawHtml(spendTimeData)}
-              </div>
-            </div>
-          </div>
-          <div class="wrap-daily">
-            <strong class="daily-title">
-              <label for="score">집중 점수</label>
-            </strong>
-            <div class="daily-detail">
-              <input
-                type="text"
-                id="score"
-                class="tf-daily tf-score"
-                placeholder="99.9" />
-              <span class="txt-score">점</span>
-            </div>
-          </div>
-          <div class="wrap-daily">
-            <strong class="daily-title">
-              <label for="hurdle">허들</label>
-            </strong>
-            <div class="daily-detail">
-              <input
-                type="text"
-                id="hurdle"
-                class="tf-daily"
-                placeholder="방해된 요소를 입력해주세요." />
-              <div class="wrap-hurdles"></div>
-            </div>
-          </div>
-          <div class="wrap-daily">
-            <strong class="daily-title">
-              <label for="retrospect">반성</label>
-            </strong>
-            <div class="daily-detail">
-              <textarea
-                id="retrospect"
-                class="tf-retrospect"
-                name="retrospect"
-                maxlength="100"
-                placeholder="100자 이내로 입력해 주세요."
-                rows="5"></textarea>
+        </div>
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <span class="txt-essential"
+              >*
+              <span class="blind">필수</span>
+            </span>
+            소요시간
+          </strong>
+          <div class="daily-detail">
+            <div class="wrap-spend">
+              ${RadioBox.drawHtml(spendTimeData)}
             </div>
           </div>
         </div>
-      </form>
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <label for="score">집중 점수</label>
+          </strong>
+          <div class="daily-detail">
+            <input
+              type="text"
+              id="score"
+              class="tf-daily tf-score"
+              placeholder="99.9" />
+            <span class="txt-score">점</span>
+          </div>
+        </div>
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <label for="hurdle">허들</label>
+          </strong>
+          <div class="daily-detail">
+            <input
+              type="text"
+              id="hurdle"
+              class="tf-daily"
+              placeholder="방해된 요소를 입력해주세요." />
+            <div class="wrap-hurdles"></div>
+          </div>
+        </div>
+        <div class="wrap-daily">
+          <strong class="daily-title">
+            <label for="retrospect">반성</label>
+          </strong>
+          <div class="daily-detail">
+            <textarea
+              id="retrospect"
+              class="tf-retrospect"
+              name="retrospect"
+              maxlength="100"
+              placeholder="100자 이내로 입력해 주세요."
+              rows="5"></textarea>
+          </div>
+        </div>
+      </div>
+    </form>
     `;
 };
 
--- src/js/modal/ModalLayout.js
+++ src/js/modal/ModalLayout.js
@@ -7,6 +7,7 @@ ModalLayout.prototype.create = function (modalContentHtml) {
   this.element.innerHTML = this.drawHtml(this.modalTitle);
 
   this.contentElement = document.querySelector('.popup-content-body');
+  this.btnFooterSave = document.querySelector('.btn-footer-save');
   this.btnFooterCloseElement = document.querySelector('.btn-footer-close');
 
   this.contentElement.innerHTML = modalContentHtml;
@@ -16,18 +17,20 @@ ModalLayout.prototype.drawHtml = function (title) {
   return `
       <div class="popup">
         <div class="popup-content">
-          <div class="popup-content-header">
-            <h4 class="popup-title">${title}</h4>
-          </div>
-          <div class="popup-content-body"></div>
-          <div class="popup-content-footer">
-            <button type="submit" class="btn-footer btn-footer-save">
-              저장
-            </button>
-            <button type="button" class="btn-footer btn-footer-close">
-              닫기
-            </button>
-          </div>
+          
+            <div class="popup-content-header">
+              <h4 class="popup-title">${title}</h4>
+            </div>
+            <div class="popup-content-body"></div>
+            <div class="popup-content-footer">
+              <button type="button" class="btn-footer btn-footer-save">
+                저장
+              </button>
+              <button type="button" class="btn-footer btn-footer-close">
+                닫기
+              </button>
+            </div>
+          
         </div>
       </div>
     `;
